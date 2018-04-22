10650997e04e2ea0c2bab20d14de8a3b8353f04a
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-04-23 00:39:06 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -7,118 +7,118 @@ class CircularInt {
 public:
 	
 	//Constructor
-    CircularInt(int lower, int upper);
+    CircularInt(int lower, int upper) {}
     
     //----Getters----//
 	
-	int getLower();						//Returns lower limit
-	int getUpper();						//Returns upper limit
-	int getValue();						//Returns value
+	int getLower() {return 1;}						//Returns lower limit
+	int getUpper() {return 1;}					//Returns upper limit
+	int getValue() {return 1;}						//Returns value
 	
 	//----Unary operators----//
 	
-	CircularInt& operator++ ();			//Prefix increment
-	CircularInt operator++ (int);		//Postfix increment
-	CircularInt& operator-- ();			//Prefix decrement
-	CircularInt operator-- (int);		//Postfix decrement
-	CircularInt operator+ () const;		//Unary plus
-	CircularInt operator- () const;		//Unary minus
+	CircularInt& operator++ () {CircularInt c(1,12); return c;}			//Prefix increment
+	CircularInt operator++ (int) {CircularInt c(1,12); return c;}		//Postfix increment
+	CircularInt& operator-- () {CircularInt c(1,12); return c;}			//Prefix decrement
+	CircularInt operator-- (int) {CircularInt c(1,12); return c;}		//Postfix decrement
+	CircularInt operator+ () const {CircularInt c(1,12); return c;}		//Unary plus
+	CircularInt operator- () const {CircularInt c(1,12); return c;}		//Unary minus
 	
 	//----Binary operators----//
 	
 	//Addition
-	CircularInt operator+ (const CircularInt& other) const; 	
-	CircularInt operator+ (int i) const; 				
+	CircularInt operator+ (const CircularInt& other) const {CircularInt c(1,12); return c;}
+	CircularInt operator+ (int i) const {CircularInt c(1,12); return c;}			
 	friend CircularInt operator+ (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 	
 	//Subtraction
-	CircularInt operator- (const CircularInt& other) const; 	
-	CircularInt operator- (int i) const; 
+	CircularInt operator- (const CircularInt& other) const {CircularInt c(1,12); return c;}
+	CircularInt operator- (int i) const {CircularInt c(1,12); return c;}
 	friend CircularInt operator- (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 	
 	//Multiplication
-	CircularInt operator* (const CircularInt& other) const; 	
-	CircularInt operator* (int i) const; 
+	CircularInt operator* (const CircularInt& other) const {CircularInt c(1,12); return c;}
+	CircularInt operator* (int i) const {CircularInt c(1,12); return c;}
 	friend CircularInt operator* (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 	
 	//Division
-	CircularInt operator/ (const CircularInt& other) const; 	
-	CircularInt operator/ (int i) const; 
+	CircularInt operator/ (const CircularInt& other) const {CircularInt c(1,12); return c;}	
+	CircularInt operator/ (int i) const {CircularInt c(1,12); return c;} 
 	friend CircularInt operator/ (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 	
 	//Modulo
-	CircularInt operator% (const CircularInt& other) const; 	
-	CircularInt operator% (int i) const; 
+	CircularInt operator% (const CircularInt& other) const {CircularInt c(1,12); return c;}	
+	CircularInt operator% (int i) const {CircularInt c(1,12); return c;}
 	friend CircularInt operator% (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 	
 	//----Bitwise operators----//
 	
 	//AND
-	CircularInt operator& (const CircularInt& other) const;				
-	CircularInt operator& (int i) const;						
+	CircularInt operator& (const CircularInt& other) const {CircularInt c(1,12); return c;}			
+	CircularInt operator& (int i) const {CircularInt c(1,12); return c;}						
 	friend CircularInt operator& (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 
 	//XOR
-	CircularInt operator^ (const CircularInt& other) const;			
-	CircularInt operator^ (int i) const;							
+	CircularInt operator^ (const CircularInt& other) const {CircularInt c(1,12); return c;}		
+	CircularInt operator^ (int i) const {CircularInt c(1,12); return c;}							
 	friend CircularInt operator^ (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 
 	//OR
-	CircularInt operator| (const CircularInt& other) const;			
-	CircularInt operator| (int i) const;							
+	CircularInt operator| (const CircularInt& other) const {CircularInt c(1,12); return c;}		
+	CircularInt operator| (int i) const {CircularInt c(1,12); return c;}						
 	friend CircularInt operator| (int i, const CircularInt& a) {CircularInt c(1,12); return c;}
 
 	//NOT
-	CircularInt operator~ () const;					
+	CircularInt operator~ () const {CircularInt c(1,12); return c;}				
 	
 	//----Relational operators----//
 	
-	bool operator== (const CircularInt& other);		//Equal to
-	bool operator!= (const CircularInt& other);		//Not equal to
-	bool operator< (const CircularInt& other);		//Lesser than
-	bool operator> (const CircularInt& other);		//Greater than
-	bool operator<= (const CircularInt& other);		//Lesser or equal
-	bool operator>= (const CircularInt& other);		//Greater or equal
+	bool operator== (const CircularInt& other) {return true;}		//Equal to
+	bool operator!= (const CircularInt& other) {return true;}		//Not equal to
+	bool operator< (const CircularInt& other) {return true;}		//Lesser than
+	bool operator> (const CircularInt& other) {return true;}		//Greater than
+	bool operator<= (const CircularInt& other) {return true;}		//Lesser or equal
+	bool operator>= (const CircularInt& other) {return true;}		//Greater or equal
 
     //----Assignment operators----//
     
     void operator=(const CircularInt& other);
     //Left shift
-    CircularInt& operator<<=(const CircularInt& other); 
+    CircularInt& operator<<=(const CircularInt& other) {CircularInt c(1,12); return c;}
     //Right shift
-    CircularInt& operator>>=(const CircularInt& other);
+    CircularInt& operator>>=(const CircularInt& other) {CircularInt c(1,12); return c;}
     
     //Addition assignment
-	CircularInt& operator+= (const CircularInt& other);		//circ + circ
-    CircularInt& operator+= (int i);						//circ + int
+	CircularInt& operator+= (const CircularInt& other) {CircularInt c(1,12); return c;}	//circ + circ
+    CircularInt& operator+= (int i) {CircularInt c(1,12); return c;}						//circ + int
     
     //Subtraction assignment
-	CircularInt& operator-= (const CircularInt& other);		//circ - circ
-    CircularInt& operator-= (int i);						//circ - int
+	CircularInt& operator-= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ - circ
+    CircularInt& operator-= (int i) {CircularInt c(1,12); return c;}						//circ - int
 
 	//Multiplication assignment
-	CircularInt& operator*= (const CircularInt& other);		//circ * circ
-    CircularInt& operator*= (int i);						//circ * int
+	CircularInt& operator*= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ * circ
+    CircularInt& operator*= (int i) {CircularInt c(1,12); return c;}						//circ * int
     
 	//Division assignment
-	CircularInt& operator/= (const CircularInt& other);		//circ / circ
-    CircularInt& operator/= (int i);						//circ / int
+	CircularInt& operator/= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ / circ
+    CircularInt& operator/= (int i) {CircularInt c(1,12); return c;}						//circ / int
 	
 	//Modulo assignment
-	CircularInt& operator%= (const CircularInt& other);		//circ % circ
-    CircularInt& operator%= (int i);						//circ % int
+	CircularInt& operator%= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ % circ
+    CircularInt& operator%= (int i) {CircularInt c(1,12); return c;}						//circ % int
     
     //AND assignment
-	CircularInt& operator&= (const CircularInt& other);		//circ & circ
-    CircularInt& operator&= (int i);						//circ & int
+	CircularInt& operator&= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ & circ
+    CircularInt& operator&= (int i) {CircularInt c(1,12); return c;}						//circ & int
     
     //XOR assignment
-	CircularInt& operator^= (const CircularInt& other);		//circ ^ circ
-    CircularInt& operator^= (int i);						//circ ^ int
+	CircularInt& operator^= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ ^ circ
+    CircularInt& operator^= (int i) {CircularInt c(1,12); return c;}						//circ ^ int
     
     //OR assignment
-	CircularInt& operator|= (const CircularInt& other);		//circ | circ
-    CircularInt& operator|= (int i);						//circ | int
+	CircularInt& operator|= (const CircularInt& other) {CircularInt c(1,12); return c;}		//circ | circ
+    CircularInt& operator|= (int i) {CircularInt c(1,12); return c;}						//circ | int
     
     friend ostream& operator<< (ostream &output, const CircularInt& C) 
     { 
